74fe6d291a76f13a34ff49de232ce88a39aeaac3
Andre <andreluizfrezarinidiasdasilva@gmail.com>
2022-03-07 20:49:01 -0300

    main

--- ex01.py
+++ ex01.py
@@ -11,8 +11,8 @@ media = (nota1 + nota2) / 2
 print(f'\nA primeira nota de {nome} é de: {nota1}\nA segunda nota de {nome} é de: {nota2}\nA média de {nome} é de: {media}')
 
 if media >= 7:
-    print(f'\n{nome} passou em {curso} com uma média de: {media:.5} PARABÉNS!!!')
+    print(f'\n{nome} passou em {curso} com uma média de: {media} PARABÉNS!!!')
 elif media >= 5 and media < 7:
-    print(f'\n{nome} ficou de recuperação em {curso} com uma média de: {media:.5} FAÇA O EXAME FINAL!!!')
+    print(f'\n{nome} ficou de recuperação em {curso} com uma média de: {media} FAÇA O EXAME FINAL!!!')
 else:
-    print(f'\n{nome} não passou em {curso} com uma média de: {media:.5} ESTUDE MAIS!!!')
+    print(f'\n{nome} não passou em {curso} com uma média de: {media} ESTUDE MAIS!!!')

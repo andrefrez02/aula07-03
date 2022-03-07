1467fffb53010116e756bfd38e7e17b24398a57c
Andre <andreluizfrezarinidiasdasilva@gmail.com>
2022-03-07 20:37:23 -0300

    media em pythob

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import random
 
-var = random.randint(5, 10)
-print(var)
+var = random.randint(0, 9)
+var2 = random.randint(0, 12)
+var3 = random.randint(0, 24)
+
+#print('Os valores são: ', var, ', ', var2, ', ', var3)
+#print('Os valores são: %s, %s, %s ' % (var, var2, var3))
+print(f'Os valores são: {var}, {var2}, {var3}')
